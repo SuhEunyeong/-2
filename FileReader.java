00ea2ada6ee8e1c98ff6334625243a9a9b18f8e0
hozumi <tvxqg5jyj@gmail.com>
2019-10-07 13:32:40 +0900

    FileReader  생성

--- FileReader.java
+++ FileReader.java
@@ -1,4 +1,4 @@
-
+import java.util.HashMap;
 /**
  * Write a description of class FileReader here.
  *
@@ -7,5 +7,9 @@
  */
 public class FileReader
 {
-    
+    public static void main(String[] args){
+        FileReader fin = new FileReader("c:\temp\inputData20191007.txt");
+        HashMap<String, Integer> h = new HashMap<String, Integer>();
+        
+    }
 }
